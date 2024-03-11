f23c4cb877f8401010c7098c2343c5af5d987f29
gldcosta <163041371+gldcosta@users.noreply.github.com>
2024-03-11 16:09:06 -0300

    Add files via upload

---
+++ jdk.js
@@ -0,0 +1,11 @@
+<script>
+  window.fbAsyncInit = function () {
+    // JavaScript SDK configuration and setup
+    FB.init({
+      appId:    '911537883999452', // Meta App ID
+      cookie:   true, // enable cookies
+      xfbml:    true, // parse social plugins on this page
+      version:  'v19.0' //Graph API version
+    });
+  };
+</script>
